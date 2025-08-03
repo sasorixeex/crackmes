a575df408035b87b2e445db5da08c8abfe3bee34
sasorixeex <vanhoutterayan@gmail.com>
2025-08-03 14:47:30 +0200

    CrackMe2
    change le code pour avoir 100% de chance de gagner

---
+++ crackMe2/calculator.py
@@ -0,0 +1,9 @@
+reponse = input("choisis entre pierre,papier,ciseau svp ")
+
+if reponse == "pierre":
+    print("papier gagner")
+if reponse == "ciseaux":
+    print("pierre gagner")
+if reponse == "papier":
+    print("ciseaux gagner")
+result = input("press entrer for exit")
